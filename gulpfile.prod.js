340693f44332f6e1b103e6b80b2c736ee02833f7
Steinhof <steinhof808@gmail.com>
2019-09-22 03:39:15 +0300

    config

--- gulpfile.prod.js
+++ gulpfile.prod.js
@@ -6,6 +6,7 @@ const critical = require('critical').stream;
 const imagemin = require('gulp-imagemin');
 const webpack = require('webpack');
 const ts = require('gulp-typescript');
+const exit = require('gulp-exit');
 const SWInjectFiles = require('./config/SWInjectFiles');
 
 /* File paths */
@@ -203,4 +204,5 @@ gulp.task(
         'COPY',
         'TSDIST',
     ),
+    exit(),
 );
